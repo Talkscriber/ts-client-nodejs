c6cf477c0554b46ec9947ffa2206121e93bdc524
Aidin Faghfouri <aidin@talkscriber.com>
2024-12-30 01:48:56 -0800

    chore: update API key and adjust error handling in client example

--- examples/talkscriber_client.ts
+++ examples/talkscriber_client.ts
@@ -33,7 +33,8 @@ async function streamAudioData(audioData: Float32Array, chunkSize: number, sampl
 async function main() {
     // Create TalkscriberTranscriptionService instance
     const talkscriber = new TalkscriberTranscriptionService({
-        apiKey: 'YOUR_API_KEY_HERE', // Replace this with your actual Talkscriber API key
+        //apiKey: 'YOUR_API_KEY_HERE', // Replace this with your actual Talkscriber API key
+        apiKey: 'ALLbolf7H9nAo88ypkfwYLytOH9fosKMXpZcc-uZlhA',
         language: 'en', // This is now optional, 'en' is the default
         endpoint: 'wss://api.talkscriber.com:9090', // This is now optional, the shown value is the default
         onTranscription: (text: string) => {
@@ -60,9 +61,9 @@ async function main() {
                 console.error('The connection was closed. Please check your internet connection and try again.');
             }
         }
-        talkscriber.close();
-        process.exit(1);
     }
+    talkscriber.close();
+    process.exit(1);
 }
 
 // Handle cleanup
