83d138a6db6225e0c49c1a381be2616cc47f8fe9
Aidin Faghfouri <aidin@talkscriber.com>
2024-12-30 02:21:57 -0800

    refactor: Update import to use @talkscriber-npm/ts-client package

--- examples/talkscriber_client.ts
+++ examples/talkscriber_client.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import { decode } from 'wav-decoder';
-import { TalkscriberTranscriptionService } from '../src/TalkscriberTranscriptionService';
+//import { TalkscriberTranscriptionService } from '../src/TalkscriberTranscriptionService';
+import { TalkscriberTranscriptionService } from '@talkscriber-npm/ts-client';
 
 const audioFilePath = './examples/sample.wav';
 
